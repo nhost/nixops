153a0d0eef433b0baa96ef7bce122d11a8f99fcd
David Barroso <dbarrosop@dravetech.com>
2024-07-27 19:26:55 +0200

    asd

--- overlays/nhost-cli.nix
+++ overlays/nhost-cli.nix
@@ -1,22 +1,22 @@
 { final }:
 let
-  version = "v1.18.0";
+  version = "v1.20.0";
   dist = {
     aarch64-darwin = rec {
       url = "https://github.com/nhost/cli/releases/download/${version}/cli-${version}-darwin-arm64.tar.gz";
-      sha256 = "1r94lqxhjb2far3w7pc7qwjy31047r49f105xz953v044fz2is7d";
+      sha256 = "1k5bgi7nqpnlpb3fq03hihwk6qvk4ryh65ymw8als6mb2r92xcfd";
     };
     x86_64-darwin = rec {
       url = "https://github.com/nhost/cli/releases/download/${version}/cli-${version}-darwin-amd64.tar.gz";
-      sha256 = "17w1jhn1zy4pvnbwky0p80kqggwfr0nn4jmv2860hgf8ar8h293l";
+      sha256 = "07vfy87j3875fbbr5ssn0grnijiq3a7r7n0kjbh09dcig789a184";
     };
     aarch64-linux = rec {
       url = "https://github.com/nhost/cli/releases/download/${version}/cli-${version}-linux-arm64.tar.gz";
-      sha256 = "1gj1qrsw9d9vbiqlgz4ds8wkp2bri19kj503m2fj863bzl16jkxh";
+      sha256 = "1c5ma204dv75q21gvlx57h30ahgxajbs0v9sfzhxc17m8nx2yi3g";
     };
     x86_64-linux = rec {
       url = "https://github.com/nhost/cli/releases/download/${version}/cli-${version}-linux-amd64.tar.gz";
-      sha256 = "0fsdnys74n8wppljsp4hmq5bqdn3c98p2hgqsifxj7h7kfjrzyd8";
+      sha256 = "0mn5yvpgj4ldwfvanr9k9inj78s9lvpb6jmrz6561cvz19ylhwii";
     };
   };
 
@@ -45,10 +45,10 @@ final.stdenvNoCC.mkDerivation rec {
     mkdir -p $out/bin
     mv cli $out/bin/nhost
 
-    installShellCompletion --cmd nhost \
-      --bash <($out/bin/nhost completion bash) \
-      --fish <($out/bin/nhost completion fish) \
-      --zsh <($out/bin/nhost completion zsh)
+    # installShellCompletion --cmd nhost \
+    #   --bash <($out/bin/nhost completion bash) \
+    #   --fish <($out/bin/nhost completion fish) \
+    #   --zsh <($out/bin/nhost completion zsh)
 
     runHook postInstall
   '';
