57b426f893b11ad1711783a5fda7409f51e73984
David Barroso <dbarrosop@dravetech.com>
2025-06-12 23:09:17 +0200

    asd

--- lib/lib.nix
+++ lib/lib.nix
@@ -1,5 +1,20 @@
 { pkgs, nix2containerPkgs }:
+let
+  dockerImageFn =
+    { ... }@args:
+    nix2containerPkgs.nix2container.buildImage {
+      inherit args;
+    };
+in
 {
   go = import ./go/go.nix { inherit pkgs nix2containerPkgs; };
   nix = import ./nix/nix.nix { inherit pkgs; };
+
+  docker-image =
+    { ... }@args:
+    pkgs.runCommand "image-as-dir" { } ''
+      ${(dockerImageFn {
+        inherit args;
+      }).copyTo}/bin/copy-to dir:$out
+    '';
 }
