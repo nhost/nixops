5ade680b46b77ac2227031299119c9f657456104
David Barroso <dbarrosop@dravetech.com>
2025-07-26 07:20:51 +0200

    asd

--- flake.nix
+++ flake.nix
@@ -66,7 +66,7 @@
               # postgresql_17_5-client
               # postgresql_14_18
               postgresql_15_13
-              # postgresql_16_9
+              postgresql_16_9
               # postgresql_17_5
             ];
           };
